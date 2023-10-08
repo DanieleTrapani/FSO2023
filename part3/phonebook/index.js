01f7bedb1dfc4cd2de0d88cea475a63b30e46933
Daniele Trapani <daniele.trapani96@gmail.com>
2023-10-08 17:26:21 +0200

    basic express app

---
+++ part3/phonebook/index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(express.json());
+// app.use(morgan('tiny'));
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+
+let people = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(people);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = people.find(p => p.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'data missing'
+        });
+    }
+    if (people.some(p => p.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+    const person = {
+        id: Math.floor(Math.random() * 100000),
+        name: body.name,
+        number: body.number
+    };
+    people = people.concat(person);
+    res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    people = people.filter(p => p.id !== id);
+    res.status(204).end();
+});
+
+app.get('/info', (req, res) => {
+    const string = `Phonebook has info for ${people.length} people<br>` + new Date();
+    res.send(string);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
